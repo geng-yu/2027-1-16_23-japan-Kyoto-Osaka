50604ecffef3ab97aea289d68df094dc8fc275b0
geng-yu <ss8611ss@hotmail.com>
2026-09-15 16:00:17 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -47,7 +47,7 @@ def show():
 """)
     st.link_button("📍 京都拉麵小路", get_gmap_link("Kyoto Ramen Koji", "walking"))
 
-    show_food_table("京都飯店")
+    show_food_table("京都飯店-吃")
     show_food_table("京都飯店-逛")
 
     
